e76a36acae4636df6dc878b80a8bbb6c8bdcfb46
Jung Gyu Yoon <yjg30737@gmail.com>
2022-05-24 15:53:40 +0900

    Add new function

--- svghelper/main.py
+++ svghelper/main.py
@@ -1,7 +1,8 @@
+import os
 import xml.etree.ElementTree as ET
 
 
-def set_svg_color(input_file: str, output_file: str, replace_color: str):
+def set_color(input_file: str, output_file: str, replace_color: str):
     tree = ET.parse(input_file)
     root = tree.getroot()
     for el in root.iter():
@@ -10,3 +11,11 @@ def set_svg_color(input_file: str, output_file: str, replace_color: str):
             el.set('fill', replace_color)
     with open(output_file, 'wb') as f:
         tree.write(f)
+
+def set_color_to_files(input_dir: str, output_dir: str, replace_color: str):
+    filenames = [os.path.join(os.path.abspath(input_dir), filename)
+                 for filename in os.listdir(input_dir)]
+
+    for filename in filenames:
+        set_color(filename, os.path.join(os.path.join(os.getcwd(), output_dir),
+                                         os.path.basename(filename)), replace_color)
